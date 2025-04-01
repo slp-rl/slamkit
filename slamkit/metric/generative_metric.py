d59f69e6c0575cbd0a6de78f1fc00edfdcdf1c5e
Avishai Elmakies <avishai.elma@cs.huji.ac.il>
2025-04-01 11:59:39 +0300

    remove print, remove shadow

--- slamkit/metric/generative_metric.py
+++ slamkit/metric/generative_metric.py
@@ -156,7 +156,7 @@ def llm_as_judge(model: SpeechLM, data_path: str, batch_size: int, whisper_model
         prompts.extend(audio)
         prompt_audio_transcriptions = whisper_pipeline([sample.cpu().numpy() for sample in audio], batch_size=len(audio))
         generated_audio_transcriptions = whisper_pipeline([gen.cpu().numpy() for gen in gen_res], batch_size=len(gen_res))
-        prompt_audio_transcriptions = [res_text["text"] if audio.shape[-1] > 0 else "" for audio, res_text in zip(audio, prompt_audio_transcriptions)]
+        prompt_audio_transcriptions = [res_text["text"] if smaple.shape[-1] > 0 else "" for smaple, res_text in zip(audio, prompt_audio_transcriptions)]
         generated_audio_transcriptions = [res_text["text"] if gen.shape[-1] > 0 else "" for gen, res_text in zip(gen_res, generated_audio_transcriptions)]
         text = [
             instruction.replace("[prompt_audio_transcription]", prompt_audio_transcription).replace("[generated_audio_transcription]", generated_audio_transcription) for 
@@ -168,5 +168,5 @@ def llm_as_judge(model: SpeechLM, data_path: str, batch_size: int, whisper_model
     res = judge(texts)
     res = list(filter(lambda x: x is not None, res))
     text_res = [(p_text,g_text) for p_text, g_text, r in zip(prompt_texts, gen_texts, res)]
-    print("got response for {} out of {}".format(len(res), len(dataset)))
+    logger.info("got response for {} out of {}".format(len(res), len(dataset)))
     return {'llm_as_judge': torch.as_tensor(res).float().mean().item(), "generate": gen, "prompts": prompts, "text_res": text_res}
